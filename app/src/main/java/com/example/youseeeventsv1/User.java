74051310fd5dc2d6497d9f4871116ec319846326
youseeevents <yousee.eventsby8@gmail.com>
2019-05-01 16:28:41 -0700

    initial commit

---
+++ app/src/main/java/com/example/youseeeventsv1/User.java
@@ -0,0 +1,20 @@
+package com.example.youseeeventsv1;
+
+public class User {
+
+    private String email;
+    private String password;
+
+    public User(String e, String p){
+        this.email = e;
+        this.password = p;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+}
