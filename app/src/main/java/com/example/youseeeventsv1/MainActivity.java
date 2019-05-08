acaf040ecf81ee039510133d4d7693d1f4467c62
youseeevents <yousee.eventsby8@gmail.com>
2019-05-08 15:33:14 -0700

    adding fragment?

--- app/src/main/java/com/example/youseeeventsv1/MainActivity.java
+++ app/src/main/java/com/example/youseeeventsv1/MainActivity.java
@@ -3,14 +3,11 @@ package com.example.youseeeventsv1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.view.MenuItem;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,13 +24,13 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.menu_test_1:
-                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                        Fragment home_fragment = new HomeFragment();
                         break;
                     case R.id.menu_test_2:
                         startActivity(new Intent( MainActivity.this, MainActivity.class));
                         break;
                     case R.id.menu_test_3:
-
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
                         break;
                 }
                 return true;
