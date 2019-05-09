97f6e8b7345f497e42e5e1875e58add9152eecf5
youseeevents <yousee.eventsby8@gmail.com>
2019-05-08 18:10:08 -0700

    added SignUpActivity and updates to xml

--- app/src/main/java/com/example/youseeeventsv1/MyEventsFragment.java
+++ app/src/main/java/com/example/youseeeventsv1/MyEventsFragment.java
@@ -6,12 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
 
 /**
  * Created by Belal on 1/23/2018.
  */
 
 public class MyEventsFragment extends Fragment {
+
+    private FirebaseDatabase firebaseRef = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseRef = firebaseRef.getReference();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,6 +29,23 @@ public class MyEventsFragment extends Fragment {
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
+
         return inflater.inflate(R.layout.fragment_my_events, null);
+    }
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        Button testButton = (Button) getView().findViewById(R.id.test_button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeDummyEvent();
+            }
+        });
+    }
+
+    private void makeDummyEvent() {
+        System.out.println("Dummy event made");
+        Event dummy = new Event("Test", "Test Desc",new Date(5,8,2019), 0, "Geisel 1W", null);
+        databaseRef = firebaseRef.getReference().child(dummy.getName());
+        databaseRef.setValue(dummy);
     }
 }
