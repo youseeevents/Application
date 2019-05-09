87235bce0a31c7712ea2d115e703870a35f35c6d
Andrew Muro <muro_andrew@yahoo.com>
2019-05-08 18:43:40 -0700

    ScrapeReader and Event progress

--- app/src/main/java/com/example/youseeeventsv1/Event.java
+++ app/src/main/java/com/example/youseeeventsv1/Event.java
@@ -7,25 +7,30 @@ public class Event {
     private int eventId;
     private String name;
     private String eventDescription;
-    private int eventCounter;
+    private int eventCounterGoing;
+    private int eventCounterInterested;
 
     private Date date;
-    private int time;
+    private String time;
     private String location;
 
     private String[] tags;
 
-    public Event(String name, String eventDescription, Date date, int time, String location, String[]  tags){
+    private static int eventIDCounter = 0;
+
+    public Event(String name, String eventDescription, Date date, String time, String location, String[]  tags){
         // eventId will be set up by the database... figure that out
         this.name = name;
         this.eventDescription = eventDescription;
-        this.eventCounter = 0;
 
         this.date = date;
         this.time = time;
         this.location = location;
-
+        this.eventCounterGoing = 0;
+        this.eventCounterInterested = 0;
         this.tags = tags;
+        this.eventId = eventIDCounter;
+        eventIDCounter++;
     }
     /** SETTERS */
     public void setEventId(int eventId) {
@@ -40,15 +45,19 @@ public class Event {
         this.eventDescription = eventDescription;
     }
 
-    public void setEventCounter(int eventCounter) {
-        this.eventCounter = eventCounter;
+    public void setEventCounterGoing(int eventCounterGoing) {
+        this.eventCounterGoing = eventCounterGoing;
+    }
+
+    public void setEventCounterInterested(int eventCounterInterested) {
+        this.eventCounterInterested = eventCounterInterested;
     }
 
     public void setDate(Date date) {
         this.date = date;
     }
 
-    public void setTime(int time) {
+    public void setTime(String time) {
         this.time = time;
     }
 
@@ -73,15 +82,19 @@ public class Event {
         return eventDescription;
     }
 
-    public int getEventCounter() {
-        return eventCounter;
+    public int getEventCounterGoing() {
+        return eventCounterGoing;
+    }
+
+    public int getEventCounterInterested() {
+        return eventCounterInterested;
     }
 
     public Date getDate() {
         return date;
     }
 
-    public int getTime() {
+    public String getTime() {
         return time;
     }
 
