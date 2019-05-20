c0e173d58cddebf89897a1646cd0dba7d2702ff4
Tai <t8white@ucsd.edu>
2019-05-20 11:33:05 -0700

    tests added

---
+++ app/src/androidTest/java/com/example/youseeeventsv1/LoginActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.youseeeventsv1;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.action.ViewActions.click;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Test
+    public void testLogin() {
+
+        onView(withId(R.id.email)).perform(typeText("testemail@website.com"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("testpassword"), closeSoftKeyboard());
+        onView(withId(R.id.sign_in_button)).perform(click());
+
+    }
+
+}
---
+++ app/src/androidTest/java/com/example/youseeeventsv1/SignUpActivityTest.java
@@ -0,0 +1,31 @@
+package com.example.youseeeventsv1;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class SignUpActivityTest {
+
+    @Test
+    public void testSignUp() {
+
+        onView(withId(R.id.email)).perform(typeText("testemail@website.com"), closeSoftKeyboard());
+        onView(withId(R.id.username)).perform(typeText("testuser"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("testpassword"), closeSoftKeyboard());
+        onView(withId(R.id.sign_up_button)).perform(click());
+
+    }
+}
