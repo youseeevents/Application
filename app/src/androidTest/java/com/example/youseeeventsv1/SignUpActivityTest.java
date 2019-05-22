41fee86715a641057f521a6ffb57997bfdde661e
Tai <t8white@ucsd.edu>
2019-05-22 15:13:18 -0700

    test change

--- app/src/androidTest/java/com/example/youseeeventsv1/SignUpActivityTest.java
+++ app/src/androidTest/java/com/example/youseeeventsv1/SignUpActivityTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
