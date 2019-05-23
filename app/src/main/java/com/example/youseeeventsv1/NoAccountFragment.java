944acaafaa0bb88eae34b642d5e9ac3bacc44aed
youseeevents <yousee.eventsby8@gmail.com>
2019-05-22 18:24:02 -0700

    fixed NoAccountFragment error (not extending)

--- app/src/main/java/com/example/youseeeventsv1/NoAccountFragment.java
+++ app/src/main/java/com/example/youseeeventsv1/NoAccountFragment.java
@@ -2,11 +2,12 @@ package com.example.youseeeventsv1;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class NoAccountFragment {
+public class NoAccountFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
