a96908089078954426b2d5a5c27863c6e9c40821
Adrian Mendoza <adrian24men@gmail.com>
2019-05-29 22:33:37 -0700

    Finished create event xml

--- app/src/main/java/com/example/youseeeventsv1/create_event.java
+++ app/src/main/java/com/example/youseeeventsv1/create_event.java
@@ -6,8 +6,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class create_event extends AppCompatActivity {
+
+    ImageButton createEventImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,14 @@ public class create_event extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        createEventImageButton = (ImageButton) findViewById(R.id.image_createEvent);
+        createEventImageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(create_event.this, "It works", Toast.LENGTH_LONG).show();
+
             }
         });
     }
