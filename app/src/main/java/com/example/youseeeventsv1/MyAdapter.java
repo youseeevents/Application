bb0b68b802be6fb441c327c04261bbadfe93870b
youseeevents <yousee.eventsby8@gmail.com>
2019-06-03 20:29:23 -0700

    functioning delete? probably

--- app/src/main/java/com/example/youseeeventsv1/MyAdapter.java
+++ app/src/main/java/com/example/youseeeventsv1/MyAdapter.java
@@ -1,5 +1,6 @@
 package com.example.youseeeventsv1;
 
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +11,12 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -63,27 +70,6 @@ class MyAdapter extends android.support.v7.widget.RecyclerView.Adapter {
             delete_button = view.findViewById(R.id.delete_button);
             edit_button = view.findViewById(R.id.edit_button);
             cancel_button = view.findViewById(R.id.cancel_button);
-
-            delete_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                }
-            });
-
-            edit_button.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-
-                }
-            });
-
-            cancel_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    organizer_buttons.setVisibility(View.GONE);
-                }
-            });
         }
 
         public void bind(final Event item, final OnItemClickListener listener){
@@ -100,6 +86,35 @@ class MyAdapter extends android.support.v7.widget.RecyclerView.Adapter {
                 @Override
                 public boolean onLongClick(View v) {
                     organizer_buttons.setVisibility(View.VISIBLE);
+                    delete_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("Events")
+                                    .child(item.getEventId())
+                                    .removeValue();
+                            String username = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+                            FirebaseDatabase.getInstance().getReference()
+                                    .child("Users")
+                                    .child(username)
+                                    .child("created_events")
+                                    .child(item.getEventId())
+                                    .removeValue();
+                            organizer_buttons.setVisibility(View.GONE);
+                            itemView.setVisibility(View.GONE);
+                        }
+                    });
+                    edit_button.setOnClickListener(new View.OnClickListener(){
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                    cancel_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            organizer_buttons.setVisibility(View.GONE);
+                        }
+                    });
                     return listenerLong.onItemLongClick(item);
                 }
             });
